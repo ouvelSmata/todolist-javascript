4da1527f203fa9ebd903763aa51511794d4e560e
Ouvel Smata <ouvelsmata@gmail.com>
2023-01-11 10:22:51 +0000

    add search todolist

--- src/index.ts
+++ src/index.ts
@@ -8,28 +8,37 @@ function clearTodolist() {
   }
 }
 
+function addTodolist(todo: string) {
+  const trTodo = document.createElement("tr");
+
+  const tdButton = document.createElement("td");
+  trTodo.appendChild(tdButton);
+
+  const button = document.createElement("input");
+  button.type = "button";
+  button.value = "Done";
+  tdButton.appendChild(button);
+
+  const tdTodo = document.createElement("td");
+  tdTodo.textContent = todo;
+  trTodo.appendChild(tdTodo);
+
+  const todoBody = document.getElementById("todoBody");
+  todoBody.appendChild(trTodo);
+}
+
 function displayTodolist() {
   clearTodolist();
 
   for (let i = 0; i < todolist.length; i++) {
     const todo = todolist[i];
 
-    const trTodo = document.createElement("tr");
+    const filter = document.getElementById("filter") as HTMLInputElement;
+    const result = filter.value;
 
-    const tdButton = document.createElement("td");
-    trTodo.appendChild(tdButton);
-
-    const button = document.createElement("input");
-    button.type = "button";
-    button.value = "Done";
-    tdButton.appendChild(button);
-
-    const tdTodo = document.createElement("td");
-    tdTodo.textContent = todo;
-    trTodo.appendChild(tdTodo);
-
-    const todoBody = document.getElementById("todoBody");
-    todoBody.appendChild(trTodo);
+    if (todo.toLowerCase().includes(result)) {
+      addTodolist(todo);
+    }
   }
 }
 
@@ -43,6 +52,14 @@ document.forms["todoForm"].onsubmit = (event: SubmitEvent): void => {
   document.forms["todoForm"].reset();
   console.info(todolist);
 
+  displayTodolist();
+};
+
+document.getElementById("filter").onkeyup = (): void => {
+  displayTodolist();
+};
+
+document.getElementById("filter").onkeydown = (): void => {
   displayTodolist();
 };
 

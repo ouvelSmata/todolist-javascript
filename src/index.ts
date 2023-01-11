151ac4727e9a17cdcca7a3b37011dfbe476c38f9
Ouvel Smata <ouvelsmata@gmail.com>
2023-01-11 06:08:28 +0000

    add display todolist

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
 const todolist: string[] = [];
 
+function clearTodolist() {
+  const todoBody = document.getElementById("todoBody");
+
+  while (todoBody.firstChild) {
+    todoBody.removeChild(todoBody.firstChild);
+  }
+}
+
+function displayTodolist() {
+  clearTodolist();
+
+  for (let i = 0; i < todolist.length; i++) {
+    const todo = todolist[i];
+
+    const trTodo = document.createElement("tr");
+
+    const tdButton = document.createElement("td");
+    trTodo.appendChild(tdButton);
+
+    const button = document.createElement("input");
+    button.type = "button";
+    button.value = "Done";
+    tdButton.appendChild(button);
+
+    const tdTodo = document.createElement("td");
+    tdTodo.textContent = todo;
+    trTodo.appendChild(tdTodo);
+
+    const todoBody = document.getElementById("todoBody");
+    todoBody.appendChild(trTodo);
+  }
+}
+
 document.forms["todoForm"].onsubmit = (event: SubmitEvent): void => {
   event.preventDefault();
 
@@ -9,4 +42,8 @@ document.forms["todoForm"].onsubmit = (event: SubmitEvent): void => {
 
   document.forms["todoForm"].reset();
   console.info(todolist);
+
+  displayTodolist();
 };
+
+displayTodolist();
